0e97ef1ea84fbd57213d24db02aa2e7c753368b6
Maximilian Weyand <weyand@in.tum.de>
2019-11-23 20:48:21 +0100

    update

--- newFrontend/main.cpp
+++ newFrontend/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <ncurses.h>
 
-WINDOW* window = NULL;
+WINDOW* titlebar = nullptr;
+WINDOW* primary = nullptr;
+WINDOW* secondary = nullptr;
+WINDOW* command = nullptr;
+
 
 int main() {
-    initscreen();
-    noecho();
-    window = initWin();
-    keypad(window, true);
-    wprintw(window, s.c_str());
-    wrefresh(window);
+    std::string s= "Hello World \n";
 
+    initscr();
+    noecho();
+
+    //enable keylistener
+    keypad(stdscr, true);
+
+    titlebar = newwin(1,COLS-2,1,1);
+    box(stdscr, ACS_VLINE, ACS_HLINE);
+//    wrefresh(stdscr);
+//    wprintw(window, s.c_str());
+//    wrefresh(window);
+    for(;;);
     return 0;
 }
