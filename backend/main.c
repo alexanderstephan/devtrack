6e0ae5f5786f320795f0762e1046b9554a6d3781
ge75muz <ge75muz@tum.de>
2019-11-23 13:00:51 +0100

    Fixed shit

--- backend/main.c
+++ backend/main.c
@@ -4,7 +4,7 @@
 #include <sys/socket.h>
 #include <time.h>
 
-//FILE *output_file;
+FILE *output_file;
 
 typedef struct linkedList{
   int timestamp;
@@ -19,6 +19,7 @@ data_t * create_new_node(char *act, int timestamp){
     return NULL;
   strcpy(new_node->act, act);
   new_node->timestamp=timestamp;
+  new_node->next=NULL;
   return new_node;
 }
 
@@ -33,10 +34,10 @@ data_t * insert_event(data_t * head, data_t * to_insert){
   return head;
 }
 
-void insert_and_append(data_t * head, data_t * to_insert){
-  insert_event(head, to_insert);
-  /* fprintf(output_file, "%i %s\n", to_insert->act, to_insert->timestamp); */
-  /* fflush(output_file); */
+data_t * insert_and_append(data_t * head, data_t * to_insert){
+  fprintf(output_file, "%i %s\n", to_insert->timestamp, to_insert->act);
+  fflush(output_file);
+  return insert_event(head, to_insert);
 }
 
 void print_list(data_t * head){
@@ -59,12 +60,14 @@ int get_current_day(){
   //return mktime(info);
 }
 
-int main(int argv, char* args){
+int main(int argv, char** args){
   char file_to_open[128];
   sprintf(file_to_open,"/home/confringe/Hackatum/%i", get_current_day());
-  FILE *output_file=fopen(file_to_open, "a");
+  output_file=fopen(file_to_open, "a");
   printf("%s\n", file_to_open);
-  data_t * head=create_new_node("Hello World", 12);
-  head=insert_event(head, create_new_node("Hello World2", 14));
+  data_t * head=insert_and_append(NULL, create_new_node("Hello World", 12));
+  data_t * toin=create_new_node("Hello World2", 14);
+  printf("%s\n", toin->act);
+  head=insert_and_append(head, toin);
   print_list(head);
 }
